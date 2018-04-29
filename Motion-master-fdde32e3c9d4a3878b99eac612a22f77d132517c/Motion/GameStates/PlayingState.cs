7be37eea14419bd547208c3261dcb2e329235b4f
Pheubel <luca.guagliardo@live.nl>
2018-04-29 12:37:53 +0200

    Removed example code from PlayingState

--- Motion-master-fdde32e3c9d4a3878b99eac612a22f77d132517c/Motion/GameStates/PlayingState.cs
+++ Motion-master-fdde32e3c9d4a3878b99eac612a22f77d132517c/Motion/GameStates/PlayingState.cs
@@ -4,22 +4,6 @@ using System;
 namespace Motion {
     class PlayingState : GameObjectList {
         public PlayingState() {
-            ///////////
-            // Example:
-            // randomize the starting position and velocity
-            var position = new Vector2(GameEnvironment.Random.Next(100, GameEnvironment.Screen.X - 100),
-                GameEnvironment.Random.Next(100, GameEnvironment.Screen.Y - 100));
-            var velocity = new Vector2(GameEnvironment.Random.Next(-150, 150),
-                GameEnvironment.Random.Next(-150, 150));
-            // Instantiate a new ball
-            var ball = new Ball("PinkSoftColorBall", position, velocity, 30f);
-            // Add the instance of the baal to this GameObjectList
-            //Add(ball);
-            ///////////
-
-            
-
-            // step 10: randomize the position and starting velocity
 
             // The red ball moves horizontally from left to right
             Ball redBall = new Ball(
