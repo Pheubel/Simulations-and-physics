2b13a204773cf5ed1370a0ce2ee1c030e07d672a
Pheubel <luca.guagliardo@live.nl>
2018-04-29 12:35:25 +0200

    Finished assignment 1

---
+++ Motion-master-fdde32e3c9d4a3878b99eac612a22f77d132517c/Motion/GameObjects/Ball.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Motion
+{
+    class Ball : SpriteGameObject
+    {
+        private float radius;
+
+        private Vector2 _acceleration;
+        private float _gravity;
+        private float _inelasticity;
+
+        public Ball(string assetName, Vector2 position, Vector2 velocity, float radius, Vector2? acceleration = null, float gravity = 0, float inelasticity = 1)
+            : base(assetName, 0, "ball")
+        {
+            PerPixelCollisionDetection = false;
+            Position = position;
+            Velocity = velocity;
+            _acceleration = acceleration ?? Vector2.Zero;
+            _gravity = gravity;
+            _inelasticity = MathHelper.Clamp( inelasticity,0,1);
+            Origin = Center;
+            this.radius = radius;
+            scale = radius * 2 / Width;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // acceleration of the ball
+            if (velocity.X < 0)
+                velocity.X -= _acceleration.X;
+            else
+                velocity.X += _acceleration.X;
+
+            if (velocity.Y < 0)
+                velocity.Y -= _acceleration.Y;
+            else
+                velocity.Y += _acceleration.Y;
+
+
+            velocity.Y += _gravity;
+
+            KeepInScreen();
+
+            base.Update(gameTime);
+        }
+
+        private void KeepInScreen()
+        {
+            // set the fixed values to the old vectors
+            float fixedPositionX = position.X;
+            float fixedPositionY = position.Y;
+            float fixedVelocityX = velocity.X;
+            float fixedVelocityY = velocity.Y;
+
+            // the ball tries to leave on the left side of the screen.
+            if (position.X - radius < 0 && velocity.X < 0)  
+            {
+                fixedVelocityX *= -1 * _inelasticity;
+                fixedPositionX = radius;
+            }
+
+            // the ball tries to leave on the right side
+            else if (position.X + radius > GameEnvironment.Screen.X && velocity.X > 0) 
+            {
+                fixedVelocityX *= -1 * _inelasticity;
+                fixedPositionX = GameEnvironment.Screen.X - radius;
+            }
+
+            // the ball tries to leave on the top side of the screen.
+            if (position.Y - radius < 0 && velocity.Y < 0)
+            {
+                fixedVelocityY *= -1 * _inelasticity;
+                fixedPositionY = radius;
+            }
+
+            // the ball tries to leave on the bottom side
+            else if (position.Y + radius > GameEnvironment.Screen.Y && velocity.Y > 0)
+            {
+                fixedVelocityY *= -1 * _inelasticity;
+                fixedPositionY = GameEnvironment.Screen.Y - radius;
+            }
+
+            // set the velocity and position to the fixed values
+            velocity = new Vector2(fixedVelocityX, fixedVelocityY);
+            position = new Vector2(fixedPositionX, fixedPositionY);
+        }
+    }
+}
---
+++ Motion-master-fdde32e3c9d4a3878b99eac612a22f77d132517c/Motion/GameStates/PlayingState.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Motion {
+    class PlayingState : GameObjectList {
+        public PlayingState() {
+            ///////////
+            // Example:
+            // randomize the starting position and velocity
+            var position = new Vector2(GameEnvironment.Random.Next(100, GameEnvironment.Screen.X - 100),
+                GameEnvironment.Random.Next(100, GameEnvironment.Screen.Y - 100));
+            var velocity = new Vector2(GameEnvironment.Random.Next(-150, 150),
+                GameEnvironment.Random.Next(-150, 150));
+            // Instantiate a new ball
+            var ball = new Ball("PinkSoftColorBall", position, velocity, 30f);
+            // Add the instance of the baal to this GameObjectList
+            //Add(ball);
+            ///////////
+
+            
+
+            // step 10: randomize the position and starting velocity
+
+            // The red ball moves horizontally from left to right
+            Ball redBall = new Ball(
+                "RedBallX", 
+                new Vector2(0,GameEnvironment.Screen.Y/2),
+                Vector2.Zero, 
+                30,
+                new Vector2(1,0)
+            );
+
+            // The pink ball moves diagonally from the lower left to the upper right
+            Ball pinkBall = new Ball(
+                "PinkSoftColorBall",
+                new Vector2(0, GameEnvironment.Screen.Y),
+                GetVelocityToPoint(
+                    new Vector2(0, GameEnvironment.Screen.Y), 
+                    new Vector2(GameEnvironment.Screen.X, 0),
+                    100),
+                30,
+                null,
+                0.3f
+            );
+
+            // The purple ball moves from the top to the bottom.
+            Ball purpleBall = new Ball(
+                "PurpleSoftColorBall",
+                new Vector2(GameEnvironment.Screen.X / 2, 0),
+                new Vector2(0,100),
+                30,
+                null,
+                0.3f,
+                0.7f
+            );
+
+
+            // add the references to the playing state
+            Add(redBall);
+            Add(pinkBall);
+            Add(purpleBall);
+        }
+
+        /// <summary> Gets a 2-dimensional vector that will point towards the end position with a length of the given magnitude.</summary>
+        /// <param name="startPoint"> The staring point of the velocity vector.</param>
+        /// <param name="endPoint"> The point the velocity vector will aim at.</param>
+        /// <param name="magnitude"> The length of the velocity vector.</param>
+        /// <returns> Vector pointing to the end position with the length of the given magnitude</returns>
+        private Vector2 GetVelocityToPoint(Vector2 startPoint, Vector2 endPoint, float magnitude)
+        {
+            Vector2 deltaVector = endPoint - startPoint;
+            deltaVector.Normalize();
+            return deltaVector * magnitude;
+        }
+    }
+}
